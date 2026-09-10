3f634930f490ce81450598ff4a6deff52aa1673c
Peter Pütz <PtzP@rki.de>
2026-09-10 20:34:06 +0200

    latex formatting reviewed

--- scripts/create_tables_and_figures.R
+++ scripts/create_tables_and_figures.R
@@ -20,8 +20,12 @@ source(here("scripts", "analysis_setup.R"))
 
 latex_escape <- function(x) {
   replacements <- c(
-    "\\" = "\\textbackslash{}", "&" = "\\&", "%" = "\\%", "#" = "\\#",
-    "_" = "\\_", "$" = "\\$", "{" = "\\{", "}" = "\\}"
+    "&" = "\\&",
+    "%" = "\\%",
+    "#" = "\\#",
+    "_" = "\\_",
+    "{" = "\\{",
+    "}" = "\\}"
   )
   vapply(as.character(x), function(value) {
     characters <- strsplit(gsub("\n", " ", value, fixed = TRUE), "", fixed = TRUE)[[1]]
